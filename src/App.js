3ed43dc6e95f8fb541a5c08723bd116ca2912898
Ali Imran <muhammadalimran2000@gmail.com>
2021-01-05 21:39:01 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useState } from 'react';
+import WorldMap from './Components/WorldMap'
+import Header from './Components/Header'
+import SideDrawer from './Components/Drawer'
+
+
 
 function App() {
+
+  const [country,setCountry] = useState("");
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+
+    <SideDrawer/>
+    <WorldMap/>
+        
+
+    </React.Fragment>
   );
 }
 
